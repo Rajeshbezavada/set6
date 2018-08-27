f9f61fb267a03bed7431a0c2629a2de417b2488c
Rajesh bezavada <41729529+Rajeshbezavada@users.noreply.github.com>
2018-08-27 02:16:05 -0400

    Create Neaeve.java

---
+++ Neaeve.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.util.ArrayList;
+class Neaeve{
+     public static void main(String aa[]){
+     int n;
+     int sum=0;
+     Scanner s=new Scanner(System.in);
+     n=s.nextInt();
+     if(n%2!=0){
+         System.out.print(n-1);
+     }
+     else
+     {
+     	System.out.println(n);
+     }
+
+}
+}
