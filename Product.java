c351bb65b7c0290bb3c6d154a087b6688e7e8cd6
Rajesh bezavada <41729529+Rajeshbezavada@users.noreply.github.com>
2018-08-27 02:20:46 -0400

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Product
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m=sc.nextInt();
+		int k;
+		k=n*m;
+		if(k%2==0)
+		System.out.println("even");
+		else
+		System.out.println("odd");
+	}
+}
